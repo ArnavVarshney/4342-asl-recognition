e90ff6e7a2089b8dd070ae8e8547a9e7da7151eb
Abel <u3583434@connect.hku.hk>
2024-05-18 22:51:18 +0800

    first run on mac - success

---
+++ ncnn_deployment/build/CMakeFiles/mytarget.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/pregabalin/Documents/Studies/Year_3/Sem2/ELEC4342/project/technical_project/4342-asl-recognition/ncnn_deployment/ncnn.cpp" "CMakeFiles/mytarget.dir/ncnn.cpp.o" "gcc" "CMakeFiles/mytarget.dir/ncnn.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
